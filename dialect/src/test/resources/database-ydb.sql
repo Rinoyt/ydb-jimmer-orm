495a77fea491721fc30f4c63bfda9d263180a101
Rinoyt <Sasha1Sim5@gmail.com>
2026-02-04 00:08:27 +0300

    sql file for creating tables for models

---
+++ dialect/src/test/resources/database-ydb.sql
@@ -0,0 +1,14 @@
+CREATE TABLE group (
+    id Uuid,
+    name String,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE student (
+    id Uuid,
+    name String,
+    group Uuid,
+    PRIMARY KEY (id)
+);
+
+COMMIT;

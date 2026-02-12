7bd0a93cd8b65fc7129f0839714696f44b590163
Rinoyt <Sasha1Sim5@gmail.com>
2026-02-12 18:52:23 +0300

    Reworked sqlType() in YdbDialect and added resolveJdbcType()

--- dialect/src/main/java/ydb/jimmer/dialect/YdbDialect.java
+++ dialect/src/main/java/ydb/jimmer/dialect/YdbDialect.java
@@ -8,56 +8,26 @@ import org.babyfish.jimmer.sql.dialect.DefaultDialect;
 import org.jetbrains.annotations.Nullable;
 import org.json.JSONObject;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.UUID;
+import java.sql.Types;
+
+import static ydb.jimmer.dialect.constant.YdbClassMapping.classToJdbcType;
+import static ydb.jimmer.dialect.constant.YdbClassMapping.classToYdbType;
 
 public class YdbDialect extends DefaultDialect {
     @Override
     public String sqlType(Class<?> elementType) {
-        if (elementType == boolean.class || elementType == Boolean.class) {
-            return "Bool";
-        } else if (elementType == byte.class || elementType == Byte.class) {
-            return "Int8";
-        } else if (elementType == short.class || elementType == Short.class) {
-            return "Int16";
-        } else if (elementType == int.class || elementType == Integer.class || elementType == LocalTime.class || elementType == Time.class) {
-            return "Int32";
-        } else if (elementType == long.class || elementType == Long.class || elementType == BigInteger.class) {
-            return "Int64";
-        } else if (elementType == float.class || elementType == Float.class) {
-            return "Float";
-        } else if (elementType == double.class || elementType == Double.class) {
-            return "Double";
-        } else if (elementType == String.class) {
-            return "Utf8";
-        } else if (elementType == byte[].class) {
-            return "String";
-        } else if (elementType == UUID.class) {
-            return "Uuid";
-        } else if (elementType == Date.class || elementType == LocalDate.class) {
-            return "Date32";
-        } else if (elementType == LocalDateTime.class) {
-            return "Datetime64";
-        } else if (elementType == java.util.Date.class || elementType == Timestamp.class || elementType == Instant.class) {
-            return "Timestamp64";
-        } else if (elementType == BigDecimal.class) {
-            return "Decimal(22, 9)";
-        } else if (elementType == Duration.class) {
-            return "Interval64";
-        } else {
-            return null;
+        return classToYdbType.get(elementType);
+    }
+
+    @Override
+    public int resolveJdbcType(Class<?> sqlType) {
+        Integer jdbcType = classToJdbcType.get(sqlType);
+        if (jdbcType == null) {
+            return Types.OTHER;
         }
+        return jdbcType;
     }
 
     @Override
